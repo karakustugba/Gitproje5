91ddc4d135270b5fe9cbcccfe8a78361811793da
tugbakarakus <karakustugba90@gmail.com>
2023-01-26 16:38:09 +0000

    task4

--- src/gun01/Student.java
+++ src/gun01/Student.java
@@ -5,5 +5,7 @@ public class Student {
     public static void main(String[] args) {
         System.out.println(" meraba2 " );
         System.out.println("2 task yapildii");
+        System.out.println("canlar selam");
+
     }
 }
--- src/gun01/okul.java
+++ src/gun01/okul.java
@@ -3,5 +3,6 @@ package gun01;
 public class okul {
     public static void main(String[] args) {
         System.out.println("Merhaba dunya");
+        System.out.println("okuldan merhaba");
     }
 }
