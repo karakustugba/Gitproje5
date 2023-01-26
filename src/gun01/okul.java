e04e2da4c8b9047a56573cb42f053ed4a8d156ae
tugbakarakus <karakustugba90@gmail.com>
2023-01-26 17:10:25 +0000

    son hali

--- src/gun01/okul.java
+++ src/gun01/okul.java
@@ -4,5 +4,6 @@ public class okul {
     public static void main(String[] args) {
         System.out.println("Merhaba dunya");
         System.out.println("okuldan merhaba");
+        System.out.println("canlarim");
     }
 }
